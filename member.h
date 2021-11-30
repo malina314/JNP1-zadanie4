8f857318a1989e6c06a754166d29d85895073efc
Mateusz Malinowski <malina314@op.pl>
2021-11-30 21:56:09 +0100

    dokończenie implementacji i poprawki

--- member.h
+++ member.h
@@ -2,29 +2,13 @@
 #define MEMBER_H
 
 #include "treasure.h"
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 
 template<typename T, bool IsArmed>
 requires TreasureValueType<T>
-class Adventurer {
-public:
-    using strength_t = uint32_t;
-
-    static constexpr bool isArmed = IsArmed;
-
-    // ta metoda chyba nie jest "dziedziczona" przez specjalizacje
-    // todo (Mateusz): poprawić to
-    constexpr T pay() {
-        T buff = value;
-        value = 0;
-        return buff;
-    }
-
-private:
-    T value;
-    strength_t strength;
-};
+class Adventurer {}; // Klasa jest implementowana poprzez specjalizacje.
 
 template<typename T>
 requires TreasureValueType<T>
@@ -36,13 +20,20 @@ public:
 
     constexpr Adventurer() : value(0), strength(0) {}
 
+    // todo (Mateusz): sprawdzić czy tutaj nie powinno być T2
     constexpr void loot(SafeTreasure<T>&& treasure) {
         value += treasure.getLoot();
     }
 
+    constexpr T pay() {
+        T buff = value;
+        value = 0;
+        return buff;
+    }
+
 private:
     T value;
-    strength_t strength;
+    const strength_t strength;
 };
 
 template<typename T>
@@ -63,7 +54,13 @@ public:
         value += treasure.getLoot();
     }
 
-    constexpr strength_t getStrength() { return strength; }
+    constexpr strength_t getStrength() const {return strength;}
+
+    constexpr T pay() {
+        T buff = value;
+        value = 0;
+        return buff;
+    }
 
 private:
     T value;
@@ -76,35 +73,43 @@ using Explorer = Adventurer<T, false>;
 template<typename T, std::size_t CompletedExpeditions>
 requires TreasureValueType<T> && (CompletedExpeditions < 25)
 class Veteran {
-
 public:
     using strength_t = uint32_t;
 
     static constexpr bool isArmed = true;
 
-    constexpr Veteran() : value(0), completedExpeditions(CompletedExpeditions) {
-        // todo (Mateusz): fibonacci
-        strength = 0;
-    }
+    constexpr Veteran() : value(0), completedExpeditions(CompletedExpeditions),
+        strength(calcStrength()) {}
 
     template<bool isTrapped>
     constexpr void loot(Treasure<T, isTrapped>&& treasure) {
         value += treasure.getLoot();
     }
 
-    T pay() {
+    constexpr strength_t getStrength() const {return strength;}
+
+    constexpr T pay() {
         T buff = value;
         value = 0;
         return buff;
     }
 
-    constexpr strength_t getStrength() { return strength; }
-
-
 private:
     T value;
-    strength_t strength;
-    std::size_t completedExpeditions;
+    const std::size_t completedExpeditions;
+    const strength_t strength;
+
+    constexpr strength_t calcStrength() const {
+        if (CompletedExpeditions == 0) {
+            return 0;
+        }
+        strength_t a = 0, b = 1;
+        for (std::size_t i = 2; i <= CompletedExpeditions; i++) {
+            a += b;
+            std::swap(a, b);
+        }
+        return b;
+    }
 };
 
 #endif // MEMBER_H
