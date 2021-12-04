795759f2e62853581ce90ba242c0d1e198b89e15
michalMaszkowski <michalmaszkowski@gmail.com>
2021-12-04 16:35:20 +0100

    początek robienia treasure_hunt

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -5,25 +5,41 @@
 #include "member.h"
 
 template<typename T>
-concept EncounterSide = requires() {
-    //todo (Michał):
-    // zaimplementować to
-};
+concept ValidTreasure = requires() {}
+
+template<typename T>
+concept ValidMember = requires() {}
+
+template<typename T>
+concept EncounterSide = ValidMember<T> || ValidTreasure<T>;
 
 template<typename sideA, typename sideB>
 requires EncounterSide<sideA> && EncounterSide<sideB>
 class Encounter {
+public:
+    template<typename A, typename B>
+    requires ValidMember<A> && ValidMember<B>
+    constexpr void static run(Encounter<A, B> encounter) {
+        //member + member
+    }
+
+    template<typename A, typename B>
+    requires ValidTreasure<A> && ValidMember<B>
+    constexpr void static run(Encounter<A, B> encounter) {
+        //treasure + member
+    }
+
+    template<typename A, typename B>
+    requires ValidMember<A> && ValidTreasure<B>
+    constexpr void static run(Encounter<A, B> encounter) = run(Encounter(encounter.b, encounter.a));
+            //treasure + member
+private:
     sideA& a;
     sideB& b;
-
-    constexpr void run(Encounter encounter) {
-        //todo (Michał):
-        // zaimplementować to
-    }
 };
 
 // todo (wspólnie): poprawić to
-//  ale chyba najpierw trzeba zrobi wcześniejsze rzeczy
+//  ale chyba najpierw trzeba zrobi wcześniejsze rzeczy //~nie trzeba, wystarczy używać run(..) (Michał)
 //  na pewno musi być variadic template
 template<typename sideA, typename sideB>
 void expedition(Encounter<sideA, sideB>... E) {
